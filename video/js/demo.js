8d44daf765a91edf4ddb9c6c791229b2f48b360f
snickliu <15012465978@163.com>
2018-09-18 16:57:00 +0800

    视频资料

---
+++ video/js/demo.js
@@ -0,0 +1,64 @@
+
+
+
+
+
+
+
+
+/*
+http://72da.com 模板集 3800+网页模板永久免费下载，持续更新中
+http://72da.com 模板集 3800+网页模板永久免费下载，持续更新中
+http://72da.com 模板集 3800+网页模板永久免费下载，持续更新中
+http://72da.com 模板集 3800+网页模板永久免费下载，持续更新中
+http://72da.com 模板集 3800+网页模板永久免费下载，持续更新中
+http://72da.com 模板集 3800+网页模板永久免费下载，持续更新中
+http://72da.com 模板集 3800+网页模板永久免费下载，持续更新中
+http://72da.com 模板集 3800+网页模板永久免费下载，持续更新中
+http://72da.com 模板集 3800+网页模板永久免费下载，持续更新中
+http://72da.com 模板集 3800+网页模板永久免费下载，持续更新中
+*/
+
+
+
+
+
+
+
+(function(window){
+
+  var nav = document.querySelector("nav.cmn-tile-nav"),
+      nav_toggle = document.querySelector("a.nav-toggle");
+
+  nav_toggle.addEventListener("click", function(e){
+    e.preventDefault();
+    classie.toggle(nav, "open");
+  });
+
+})(window);
+
+
+
+
+
+
+
+/*
+http://72da.com 模板集 3800+网页模板永久免费下载，持续更新中
+http://72da.com 模板集 3800+网页模板永久免费下载，持续更新中
+http://72da.com 模板集 3800+网页模板永久免费下载，持续更新中
+http://72da.com 模板集 3800+网页模板永久免费下载，持续更新中
+http://72da.com 模板集 3800+网页模板永久免费下载，持续更新中
+http://72da.com 模板集 3800+网页模板永久免费下载，持续更新中
+http://72da.com 模板集 3800+网页模板永久免费下载，持续更新中
+http://72da.com 模板集 3800+网页模板永久免费下载，持续更新中
+http://72da.com 模板集 3800+网页模板永久免费下载，持续更新中
+http://72da.com 模板集 3800+网页模板永久免费下载，持续更新中
+*/
+
+
+
+
+
+
+
